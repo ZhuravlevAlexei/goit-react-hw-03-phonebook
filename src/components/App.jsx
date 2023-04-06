440f6663cb493ce2f7036014ff10b815863f8446
ZhuravlevAlexei <110524860+ZhuravlevAlexei@users.noreply.github.com>
2023-04-06 09:47:53 +0300

    interact with LocalStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,22 +7,49 @@ import Filter from './Filter/Filter';
 import ContactList from './ContactList/ContactList';
 import ContactElement from './ContactElement/ContactElement';
 
+const CONTACTS_STORAGE_KEY = 'CONTACTS';
+
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
+  componentDidMount() {
+    try {
+      const serializedState = localStorage.getItem(CONTACTS_STORAGE_KEY);
+      if (serializedState) {
+        this.setState({ contacts: JSON.parse(serializedState) });
+      } else {
+        this.setState({
+          contacts: [
+            { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+            { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+            { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+            { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+          ],
+        });
+      }
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      try {
+        const serializedState = JSON.stringify(this.state.contacts);
+        localStorage.setItem(CONTACTS_STORAGE_KEY, serializedState);
+      } catch (error) {
+        console.error(error.message);
+      }
+    }
+  }
+
   deleteContact = id => {
-    const { contacts } = this.state;
-    const foundIndex = contacts.findIndex(cont => cont.id === id);
-    contacts.splice(foundIndex, 1);
-    this.setState({ contacts: contacts });
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
   };
 
   addContact = contact => {
